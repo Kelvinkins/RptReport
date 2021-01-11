b2cea934373f44bc99489038a4c23671ae7de7bf
Kingsley <kins4swagg@gmail.com>
2021-01-11 12:06:16 +0100

    Added script

---
+++ RptReportApp/Script.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Datas] (
+    [Id]                                INT           IDENTITY (1, 1) NOT NULL,
+    [EnrolleeNumber]                    NVARCHAR (50) NULL,
+    [Company]                           NVARCHAR (50) NULL,
+    [Hospital]                          NVARCHAR (50) NULL,
+    [LastName]                          NVARCHAR (50) NULL,
+    [OtherName]                         NVARCHAR (50) NULL,
+    [Plan]                              NVARCHAR (50) NULL,
+    [FeeForService]                     NVARCHAR (50) NULL,
+    [Capitation]                        NVARCHAR (50) NULL,
+    [PremiumPerIndividual]              NVARCHAR (50) NULL,
+    [CommissionPerIndividualPercentage] NVARCHAR (50) NULL,
+    [Date]                              NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
